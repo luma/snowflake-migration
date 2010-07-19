a043a5822560f1c54d60a283a1e77596fdd11ae6
Rolly Fordham <rolly@luma.co.nz>
2010-07-19 14:09:49 +1200

    Removed old comments

--- lib/snowflake-migration/migration.rb
+++ lib/snowflake-migration/migration.rb
@@ -58,31 +58,3 @@ module Snowflake
     end # class Migration
   end # module Migration
 end # module Snowflake
-
-=begin
-
-
-17. Migrations. Just because we are schemaless on the DB side doesn't mean evolving data needs won't affect us. Create a Schema model, each element should have one.
-		We store at least:
-			In %element_class_name%::schema::%guid% (SORTED SET)	=>
-				ZADD element_class_name::schema %timestamp% "%guid%"
-				ZADD element_class_name::schema %timestamp% "%guid%"
-				ZADD element_class_name::schema %timestamp% "%guid%"
-				ZADD element_class_name::schema %timestamp% "%guid%"
-
-  In %element_class_name%::schema::%guid%	{id: %guid%, timestamp:%timestamp%, title:%title%, description:%description%}
-
-	e.g. Company.schema.first => {id: %guid%, timestamp:%timestamp%, title:%title%, description:%description%, up:%up%, down:%down%}
-			 Company.schema.automigrate
-   		 Company.schema.migrate_to(%guid%)
-   		 Company.schema.difference(%guid1%, %guid2%)
-
-		Then rake scripts to do common migration tasks and a dsl to write the actual migrations. Add "schema" as a restricted Attribute name.
-
-ZADD element_class_name::schema 1278461960 1
-ZADD element_class_name::schema 1278548360 2
-ZADD element_class_name::schema 1278634760 3
-ZADD element_class_name::schema 1278721160 4
-
-ZRANGE element_class_name::schema 0 -1
-=end
